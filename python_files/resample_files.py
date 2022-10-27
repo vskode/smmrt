3e76edb3e15671d41afb3f91f3a0150c39a39598
vskode <vkather@gmail.com>
2022-10-27 16:48:18 +0100

    fixed resampling bug; added requirements file and readme file

--- python_files/resample_files.py
+++ python_files/resample_files.py
@@ -1,8 +1,37 @@
 from pathlib import Path
 import soundfile as sf
+import librosa as lb
+import re
+import datetime as dt
 
+def create_dir_from_date(file, target_folder):
+    """
+    Create a directory tree for year, month and day, depending on the file
+    name. 
+
+    Parameters
+    ----------
+    file : pathlib.Path object
+        file path
+    target_folder : str
+        folder for resampled files
+
+    Returns
+    -------
+    pathlib.Path object
+        directory to save file in
+    """
+    datetime_str = re.findall('[0-9]+', file.stem)[-1]
+    date = dt.datetime.strptime(datetime_str, '%y%m%d%H%M%S').date()
+    new_dir = file.parent.joinpath(target_folder) \
+                .joinpath(str(date.year)) \
+                .joinpath(str(date.month)) \
+                .joinpath(str(date.day))
+    new_dir.mkdir(exist_ok=True, parents=True)
+    return new_dir
+    
 def save_resampled_file(path, target_sr, target_folder, file_ending='.wav', 
-                        **kwargs):
+                        reorder_files=False, **kwargs):
     """
     Save a file with a different sample rate compared to the original. 
     Key word arguments can be passed so that the a offset or duration can
@@ -13,19 +42,22 @@ def save_resampled_file(path, target_sr, target_folder, file_ending='.wav',
         target_sr (int): target sample rate
         target_folder (str): name of folder where files should be stored
         file_ending (str, optional): Defaults to '.wav'.
-    """
-    Path(path).joinpath(target_folder).mkdir(exist_ok=True)
-    
+        reorder_files (bool, optional): Defaults to False.
+    """    
     files = list(Path(path).glob(f'*{file_ending}'))
     
     for i, file in enumerate(files):
         audio, sr = load_audio(file, **kwargs)
-        sf.write(file.parent.joinpath(target_folder)
-                 .joinpath(file.stem+file.suffix), audio, target_sr)
+        audio = lb.resample(audio, orig_sr=sr, target_sr=target_sr)
+        if reorder_files:
+            new_dir = create_dir_from_date(file, target_folder)
+        else:
+            new_dir = file.parent.joinpath(target_folder)
+        sf.write(new_dir.joinpath(file.stem+file.suffix), audio, target_sr)
         
         # update progress
         print(r'Resampling file {}/{} from {} Hz to {} Hz | {:.3f}% completed'
-                .format(i, len(files), sr, target_sr, i/len(files)*100), 
+                .format(i+1, len(files), sr, target_sr, (i+1)/len(files)*100), 
                 end='\r')
 
 def load_audio(file, **kwargs):
@@ -40,12 +72,13 @@ def load_audio(file, **kwargs):
         tuple: audio as array and samplerate
     """
     try:
-        audio, sr = sf.read(file, **kwargs)
+        audio, sr = lb.load(file, sr=None, **kwargs)
         return audio, sr
     except Exception as e:
         print(f"{file} is corrupted. Moving on to next file", e)
     
 if __name__ == '__main__':
-    path = r'D:\5122\5122'
+    # path = r'D:\5122\5122'
     path = r'/media/vincent/TOSHIBA EXT/5122/5122'
-    save_resampled_file(path, target_sr=2000, target_folder='resampled_2kHz')
+    save_resampled_file(path, target_sr=2000, target_folder='resampled_2kHz', 
+                        reorder_files=True)
